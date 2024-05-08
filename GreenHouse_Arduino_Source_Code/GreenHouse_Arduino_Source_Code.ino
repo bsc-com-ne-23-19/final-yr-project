04f384e26576145fc8e9a3ae6823978669efaa9d
Chisale Chiwaya <bsc-com-ne-06-19@unima.ac.mw>
2024-05-08 21:01:35 +0200

    Added a heater but it's working in reverse. Needs a FIXgit add GreenHouse_Arduino_Source_Code/GreenHouse_Arduino_Source_Code.ino !

--- GreenHouse_Arduino_Source_Code/GreenHouse_Arduino_Source_Code.ino
+++ GreenHouse_Arduino_Source_Code/GreenHouse_Arduino_Source_Code.ino
@@ -1,7 +1,5 @@
-
-
 //Library inclusions
-// #include <dht.h>
+// #include <SofwareSerial.h>
 #include <DHT.h>
 #include <DHT_U.h>
 #include <LiquidCrystal.h>
@@ -9,29 +7,36 @@
 //Initializing LCD Library
 LiquidCrystal lcd(10,9,5,4,3,2);
 
+//GSM using SofwareSerial...Initiating Serial connection on the digital pins
+// SoftwareSerial gsm(6,7);
+
 //Global constants
 #define ledPin 12
 #define ldrPin A0
 #define dhtPin A1
 #define DHTTYPE DHT11
-#define relayPin 13
+#define fanRelayPin 13
+#define heaterRelayPin 11
 
 //Global variables
 float temperature;
 float humidity;
 int ldr_Value;
+int temp_min = 20;
+int temp_max = 30;
 
 //DHT variable
 DHT dht(dhtPin,DHTTYPE);
 
 void setup() 
 {
-  
+  // gsm.begin(9600); // Set baud rate since it's a serial connection
   lcd.begin(16,2); //Specify dimensions of lcd in begin()
   Serial.begin(9600);  // initialize serial communication at 9600 bits per second:
   dht.begin();
   pinMode(ledPin, OUTPUT);
-  pinMode(relayPin, OUTPUT);
+  pinMode(fanRelayPin, OUTPUT);
+  pinMode(heaterRelayPin, OUTPUT);
   delay(1000);
 }
 
@@ -71,6 +76,7 @@ void readTemp(){
 
   if (isnan(temperature) || isnan(humidity)) {
     Serial.println("Failed to read from DHT");
+    lcd_display_invalid_dht();
   }
  
   else{
@@ -89,15 +95,9 @@ void readTemp(){
     //Print both humidity and temperature on LCD
     lcdDisplay_Temp_Hum();
 
-    //Controlling the fan
-    if(temperature > 28){
-      digitalWrite(relayPin, HIGH);
-      Serial.println("FAN ON!!");
-    }
-    else{
-      digitalWrite(relayPin, LOW);
-      Serial.println("FAN OFF!!");
-    }
+    //Activate or Deactivate Actuators
+    tempControl();
+
   }
 }
 
@@ -122,3 +122,113 @@ void lcd_display_invalid_dht(){
   lcd.setCursor(0,0);
   lcd.print("Faulty DHT Sensor");
 }
+
+// Controlling heater and fan using temperature
+void tempControl(){
+  //Controlling the fan
+
+    // Right Range-----Switch fan and heater OFF
+    if((temperature > temp_min) && (temperature < temp_max)){
+      digitalWrite(fanRelayPin,LOW);
+      Serial.println("FAN OFF!!");
+      digitalWrite(heaterRelayPin,LOW);
+      Serial.println("HEATER OFF!!");
+    }
+    
+    // Temperature is high
+    else if(temperature > temp_max){
+      digitalWrite(fanRelayPin, HIGH);
+      Serial.println("FAN ON!!");
+      digitalWrite(heaterRelayPin,LOW);
+      Serial.println("HEATER OFF!!");
+    }
+
+    // Temperature is Low
+    else{
+      digitalWrite(heaterRelayPin,HIGH);
+      Serial.println("HEATER ON!!");
+      digitalWrite(fanRelayPin, LOW);
+      Serial.println("FAN OFF!!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+This Code Block begins gsm communication. Needs more research and building.
+
+
+void gsm_communication(){
+  // Check if there is anything from the Serial, and initiate gsm
+  if(Serial.available() > 0){
+      switch(Serial.read()){
+        case 's' :
+          SendMessage();
+          break;
+
+        case 'r' : 
+          ReceiveMessage();
+          break;
+      }
+  }
+
+  // Check if there is any data from the gsm and write it to the serial
+  if(gsm.available() > 0){
+    Serial.write(gsm.read());
+  }
+}
+
+void SendMessage(){
+  //Set gsm module in Send SMS mode -- I want to send SMS
+  gsm.println("AT+CMGF=1");
+  delay(1000);
+  // Punch in phone number -- To Who?
+  gsm.println("AT+CMGS=\"+265\"\r");
+  delay(1000);
+  //Write the text
+  gsm.println("Learning happens in iterations.");
+  delay(100);
+  //Exit
+  gsm.println((char) 26);
+}
+
+void RecieveMessage(){
+  gsm.println("AT+CNMI=2,2,0,0,0");
+  delay(1000);
+}
+*/
