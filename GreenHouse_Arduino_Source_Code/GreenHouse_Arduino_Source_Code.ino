b285e428ae47afba0818afd15122e4ccdd50e246
Chisale Chiwaya <bsc-com-ne-06-19@unima.ac.mw>
2024-05-08 16:06:10 +0200

    Used Adafruit DHT library. Tested LCD. Added block to check for invalid sensor readings. Changed the pin from digital to analog(Might return later.)

--- GreenHouse_Arduino_Source_Code/GreenHouse_Arduino_Source_Code.ino
+++ GreenHouse_Arduino_Source_Code/GreenHouse_Arduino_Source_Code.ino
@@ -1,27 +1,35 @@
-//#include <DHT.h>
-//#include <DHT_U.h>
+
 
 //Library inclusions
-#include <dht.h>
+// #include <dht.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include <LiquidCrystal.h>
 
 //Initializing LCD Library
 LiquidCrystal lcd(10,9,5,4,3,2);
 
-//Global variables
+//Global constants
 #define ledPin 12
 #define ldrPin A0
-#define dhtPin 11
+#define dhtPin A1
+#define DHTTYPE DHT11
 #define relayPin 13
 
+//Global variables
+float temperature;
+float humidity;
+int ldr_Value;
+
 //DHT variable
-dht DHT;
+DHT dht(dhtPin,DHTTYPE);
 
 void setup() 
 {
   
   lcd.begin(16,2); //Specify dimensions of lcd in begin()
   Serial.begin(9600);  // initialize serial communication at 9600 bits per second:
+  dht.begin();
   pinMode(ledPin, OUTPUT);
   pinMode(relayPin, OUTPUT);
   delay(1000);
@@ -37,9 +45,9 @@ void loop()
 
 //LDR reading value and control LED
 void readLDR(){
-  int LDR_Value = analogRead(ldrPin);
+  ldr_Value = analogRead(ldrPin);
   
-  if(LDR_Value < 500)
+  if(ldr_Value < 500)
   {
     digitalWrite(ledPin,HIGH);  // Turn ON LED
     Serial.println("LED ON!!!");
@@ -56,33 +64,40 @@ void readLDR(){
 void readTemp(){
 
   //Read temperature and humidity
- // float temp = dht.readTemperature();
-  //float hum = dht.readHumidity();
-  int readings = DHT.read11(dhtPin);
+  temperature = dht.readTemperature();
+  humidity = dht.readHumidity();
 
-  //Print temperature
-  Serial.print("Temperature: ");
-  Serial.print(DHT.temperature);
-  Serial.println("C");
-  delay(500);
+  // Check if readings are valid(Working sensors)
 
-  //Print humidity
-  Serial.print("Humidity: ");
-  Serial.print(DHT.humidity);
-  Serial.println("%");    
-  delay(500);
-
-  //Print both humidity and temperature on LCD
-  lcdDisplay_Temp_Hum();
-
-  //Controlling the fan
-  if(DHT.temperature > 28){
-    digitalWrite(relayPin, HIGH);
-    Serial.println("FAN ON!!");
+  if (isnan(temperature) || isnan(humidity)) {
+    Serial.println("Failed to read from DHT");
   }
+ 
   else{
-    digitalWrite(relayPin, LOW);
-    Serial.println("FAN OFF!!");
+    //Print temperature
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println("C");
+    delay(500);
+
+    //Print humidity
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.println("%");    
+    delay(500);
+
+    //Print both humidity and temperature on LCD
+    lcdDisplay_Temp_Hum();
+
+    //Controlling the fan
+    if(temperature > 28){
+      digitalWrite(relayPin, HIGH);
+      Serial.println("FAN ON!!");
+    }
+    else{
+      digitalWrite(relayPin, LOW);
+      Serial.println("FAN OFF!!");
+    }
   }
 }
 
@@ -91,12 +106,19 @@ void lcdDisplay_Temp_Hum(){
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print("Humidity: ");
-  lcd.print(DHT.humidity);//Print Humidity
+  lcd.print(humidity);//Print Humidity
   lcd.print("%");
   lcd.setCursor(0,1);
   lcd.print("Temp: ");
-  lcd.print(DHT.temperature);//Print Temperature
+  lcd.print(temperature);//Print Temperature
   lcd.setCursor(10,1);//Move to the position
   lcd.print( (char)223);//the degree symbol
   lcd.print("C");
+}
+
+//Faulty Sensor
+void lcd_display_invalid_dht(){
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Faulty DHT Sensor");
 }
